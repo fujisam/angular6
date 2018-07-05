7a1d8280c0afc78cb7f62ed918bc5c1719b7350b
Wilson Ioshio Komatsu <wilson.komatsu@viavarejo.com.br>
2018-07-05 16:07:14 -0300

    ajustes

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -5,7 +5,7 @@ import { HeroService } from '../../services/hero.service';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: [ './dashboard.component.css' ]
 })
 export class DashboardComponent implements OnInit {
   heroes: Hero[] = [];
@@ -17,7 +17,7 @@ export class DashboardComponent implements OnInit {
   }
 
   getHeroes(): void {
-    this.heroService.getHeroes("heroes dashboard")
+    this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes.slice(1, 5));
   }
 }
